25407adb6f65abcef8bdd37ea49fbfcf95effe72
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-19 11:54:48 +0200

    [UPD] Primo lancio migrazione e seeder con faker tabella projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->name = $faker->sentence(5); 
+            $project->description = $faker->text(500);
+            $project->save();
+        }
     }
 }
